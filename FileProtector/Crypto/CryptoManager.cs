e03bdff20e2c37b213d6aa3789e5410588d1f265
zHd4 <computersystembase@gmail.com>
2024-01-17 22:45:46 +0100

    Add CryptoManager

---
+++ FileProtector/Crypto/CryptoManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace FileProtector.Crypto
+{
+    public class CryptoManager
+    {
+        private byte[] passwordBytes;
+
+        public CryptoManager(string password)
+        {
+            passwordBytes = Encoding.UTF8.GetBytes(password);
+        }
+
+        public void EncryptFile(string path)
+        {
+            AesProcessor processor = new AesProcessor(GetKey(), GetIV());
+            File.WriteAllBytes(path, processor.Encrypt(File.ReadAllBytes(path)));
+        }
+
+        public void DecryptFile(string path)
+        {
+            AesProcessor processor = new AesProcessor(GetKey(), GetIV());
+            File.WriteAllBytes(path, processor.Decrypt(File.ReadAllBytes(path)));
+        }
+
+        private byte[] GetKey()
+        {
+            return SHA256.Create().ComputeHash(passwordBytes);
+        }
+
+        private byte[] GetIV()
+        {
+            return MD5.Create().ComputeHash(passwordBytes);
+        }
+    }
+}
