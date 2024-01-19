d08ee6101e7a64650efbdf1e6e57932f817f7992
zHd4 <computersystembase@gmail.com>
2024-01-19 23:48:58 +0100

    Add FormField

---
+++ FileProtector/Models/FormField.cs
@@ -0,0 +1,22 @@
+﻿namespace FileProtector.Models
+{
+    public class FormField
+    {
+        public TextBox Field { get; }
+        public string Placeholder { get; }
+        public bool IsPassword { get; } = false;
+
+        public FormField(TextBox field, string placeholder)
+        {
+            Field = field;
+            Placeholder = placeholder;
+        }
+
+        public FormField(TextBox field, string placeholder, bool isPassword)
+        {
+            Field = field;
+            Placeholder = placeholder;
+            IsPassword = isPassword;
+        }
+    }
+}
