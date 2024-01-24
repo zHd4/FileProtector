df98971c7d15272bb8bedad9d9d99a6813b599d3
zHd4 <computersystembase@gmail.com>
2024-01-25 00:24:36 +0100

    Update icon

--- FileProtector/Properties/Resources.Designer.cs
+++ FileProtector/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace FileProtector.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap minimize_button {
             get {
                 object obj = ResourceManager.GetObject("minimize-button", resourceCulture);
