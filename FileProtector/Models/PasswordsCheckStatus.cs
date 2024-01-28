29dd090286939b9c1077e3e2a04965a2b05ccaf9
zHd4 <computersystembase@gmail.com>
2024-01-28 04:02:55 +0100

    Add PasswordsStatusCheck

---
+++ FileProtector/Models/PasswordsCheckStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace FileProtector.Models
+{
+    public class PasswordsCheckStatus
+    {
+        public bool Success { get; }
+        public string Message { get; }
+
+        public PasswordsCheckStatus(bool status, string message) 
+        {
+            Success = status;
+            Message = message;
+        }
+    }
+}
