1cbae88bcc86d3284e528d972d1e1a2cee6615dd
zHd4 <computersystembase@gmail.com>
2024-01-31 01:10:27 +0100

    Add FilesManager

---
+++ FileProtector/Forms/Main/FilesManager.cs
@@ -0,0 +1,23 @@
+﻿namespace FileProtector.Forms.Main
+{
+    internal class FilesManager
+    {
+        public static List<string> OpenSelectFilesDialog()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Multiselect = true;
+            dialog.ShowDialog();
+
+            return dialog.FileNames.ToList();
+        }
+
+        public static string OpenSelectFolderDialog()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            dialog.ShowDialog();
+            return dialog.SelectedPath;
+        }
+    }
+}
