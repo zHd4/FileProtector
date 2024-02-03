1865d49682d2cd432ec038e0f77b19caab4160aa
zHd4 <computersystembase@gmail.com>
2024-02-03 03:18:17 +0100

    Add ModalUtils

---
+++ FileProtector/Utils/ModalUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace FileProtector.Utils
+{
+    public class ModalUtils
+    {
+        public static Point GetModalWindowLocation(Form baseForm, Form modalForm)
+        {
+            int baseX = baseForm.Left + baseForm.Width / 2;
+            int baseY = baseForm.Top + baseForm.Height / 2;
+
+            int modalX = modalForm.Left + modalForm.Width / 2;
+            int modalY = modalForm.Top + modalForm.Height / 2;
+
+            return new Point(baseX - modalX, baseY - modalY);
+        }
+    }
+}
