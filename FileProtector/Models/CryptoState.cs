86679aaeab71387b52ec70a05308fa9bcfa54285
zHd4 <computersystembase@gmail.com>
2024-02-05 02:12:57 +0100

    Add CryptoState

---
+++ FileProtector/Models/CryptoState.cs
@@ -0,0 +1,11 @@
+﻿namespace FileProtector.Models
+{
+    public class CryptoState
+    {
+        public bool Completed { get; set; }
+        public int FilesCount { get; set; }
+        public int TransformedFilesCount { get; set; }
+        public string Message { get; set; } = "";
+        public List<string> Errors { get; } = new List<string>();
+    }
+}
